5b11d9fe3bb35f02d0f347707680cc0fb74d9fc1
Kian <ristorikian@gmail.com>
2022-04-05 17:04:26 +0200

    improve SEO

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
 import Layout from "../components/Layout";
 import "../styles/globals.css";
 import { ThemeProvider } from 'next-themes'
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <html lang="en">
+    <Head>
+      <title>Kian Ristori</title>
+      <meta name="description" content="I'm Full Stack Web Developer based in Italy. Welcome to my blog"/>
+    </Head>
     <ThemeProvider enableSystem={true} attribute="class">
       <Layout>
         <Component {...pageProps} />
