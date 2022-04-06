750eb091e6a0c225c29ddd890bfd90035163aaff
Kian <ristorikian@gmail.com>
2022-04-06 15:11:07 +0200

    Remove SEO from _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,10 @@
 import Layout from "../components/Layout";
 import "../styles/globals.css";
 import { ThemeProvider } from 'next-themes'
-import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <html lang="en">
-    <Head>
-      <title>Kian Ristori</title>
-      <meta name="description" content="I'm Full Stack Web Developer based in Italy. Welcome to my blog"/>
-      <meta property="og:title" content="Kian Ristori"/>
-      <meta property="og:description" content="I'm Full Stack Web Developer based in Italy. Welcome to my blog"/>
-      <meta property="og:type" content="website"/>
-    </Head>
     <ThemeProvider enableSystem={true} attribute="class">
       <Layout>
         <Component {...pageProps} />
