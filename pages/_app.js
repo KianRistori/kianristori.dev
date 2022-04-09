0d94f45f9c760f4d1252972e48a3b1fb80140d7a
Kian <ristorikian@gmail.com>
2022-04-09 13:12:23 +0200

    Firt post + favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import Layout from "../components/Layout";
 import "../styles/globals.css";
 import { ThemeProvider } from 'next-themes'
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <html lang="en">
+    <Head>
+      <link rel="shortcut icon" href="/favicon.ico" />
+    </Head>
     <ThemeProvider enableSystem={true} attribute="class">
       <Layout>
         <Component {...pageProps} />
