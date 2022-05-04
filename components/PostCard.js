966369c004ff44227737c61ecbbeabbb470efadf
Kian <ristorikian@gmail.com>
2022-05-04 17:15:01 +0200

    Change ProjectCard Style and remove view count on PostCard

--- components/PostCard.js
+++ components/PostCard.js
@@ -12,9 +12,8 @@ function PostCard({ title, date, description, image, slug }) {
             <div className="p-6 flex flex-col justify-start w-full">
               <h3 className="tracking-tight text-gray-900 dark:text-white text-2xl font-medium mb-2">{title}</h3>
               <p className="text-gray-700 dark:text-gray-400 mb-8">{description}</p>
-              <div className="flex justify-end space-x-3">
+              <div className="flex justify-end">
                 <p className="flex font-normal text-sm text-gray-500 dark:text-gray-500"><HiOutlineCalendar className="h-auto mr-1"/>{date}</p>
-                <p className="flex font-normal text-sm text-gray-500 dark:text-gray-500"><AiFillEye className="h-auto mr-1"/>{<ViewCounter slug={slug} blogPage={true}/>}</p>
               </div>
             </div>
         </div>
