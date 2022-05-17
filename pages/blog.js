de7b1a37c4bbe711433ebc65ff89e47cdff55b7d
Kian <ristorikian@gmail.com>
2022-05-17 17:29:51 +0200

    Add Search articles to blog page

--- pages/blog.js
+++ pages/blog.js
@@ -1,7 +1,15 @@
 import Head from "next/head"
 import getPosts from "../helpers/getPosts";
 import PostCard from "../components/PostCard";
+import { useState } from "react";
+
 export default function Blog({ posts }) {
+
+  const [searchValue, setSearchValue] = useState('');
+  const filterBlogPost = posts.filter((post) => 
+    post.data.title.toLowerCase().includes(searchValue.toLowerCase())
+  );
+
   return (
     <div>
       <Head>
@@ -14,7 +22,30 @@ export default function Blog({ posts }) {
       </Head>
       <h1 className="md:mt-20 mt-10 mb-6 font-bold text-5xl">Blog ✍🏻</h1>
       <p className="font-IBM text-gray-400 mb-10">Welcome to my blog page. Here you can find lot of things about coding and of me.</p>
-      {posts.map((post) => (
+      <div className="relative w-full mb-10">
+        <input
+          aria-label="Search articles"
+          type="text"
+          onChange={(e) => setSearchValue(e.target.value)}
+          placeholder="Search articles"
+          className="block w-full px-4 py-2 text-gray-900 bg-white border border-gray-200 rounded-md dark:border-gray-900 focus:ring-gray-600 focus:border-gray-700 dark:bg-gray-800 dark:text-gray-100"
+        />
+        <svg
+          className="absolute w-5 h-5 text-gray-400 right-3 top-3 dark:text-gray-300"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+          />
+        </svg>
+      </div>
+      {filterBlogPost.map((post) => (
         <PostCard
           key={post.slug}
           title={post.data.title}
