8f30058064f5851c81530325b12a110b27abff5b
Kian <ristorikian@gmail.com>
2022-05-18 17:37:40 +0200

    Change PostCard size sm device

--- components/PostCard.js
+++ components/PostCard.js
@@ -6,7 +6,7 @@ function PostCard({ title, date, description, image, slug }) {
       <Link href="/blog/[id]" as={`/blog/${slug}`}>
         <div className="flex justify-center mb-10 transition duration-500 hover:scale-105">
           <div className="flex flex-col md:flex-row rounded-lg hover:dark:bg-gray-800 shadow-lg dark:shadow-gray-800 hover:bg-gray-100 w-full">
-            <img  className="w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg" src={image} alt="post-image" />
+            <img  className="w-full h-52 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg" src={image} alt="post-image" />
             <div className="p-6 flex flex-col justify-start w-full">
               <h3 className="tracking-tight text-gray-900 dark:text-white text-2xl font-medium mb-2">{title}</h3>
               <p className="text-gray-700 dark:text-gray-400 mb-8">{description}</p>
