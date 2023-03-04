cb84a0b049aa952fc2ff454a6f908b581d81c16a
Kian <ristorikian@gmail.com>
2023-03-04 15:26:57 +0100

    Change size ogImage and add secureUrl

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -4,6 +4,7 @@ import { allPosts } from "contentlayer/generated";
 import { Mdx } from "components/mdx";
 import Balancer from 'react-wrap-balancer'
 import ViewCounter from '../../../components/view-counter';
+import Head from 'next/head';
 
 export async function generateStaticParams() {
 	return allPosts.map((post) => ({
@@ -16,8 +17,7 @@ export async function generateMetadata({ params }): Promise<Metadata | undefined
 	const ogImage = createOgImage({
 		title: post.title,
 		meta: ["kianristori.dev", post.publishedAt].join(" · "),
-	  })
-
+	})
 	if (!post) {
 		return;
 	}
@@ -38,30 +38,36 @@ export async function generateMetadata({ params }): Promise<Metadata | undefined
 			type: 'article',
 			publishedTime,
 			url: `https://kianristori.dev/blog/${slug}`,
-			images: [ogImage],
+			images: [
+				{
+					url: ogImage,
+					secureUrl: ogImage,
+					width: 1600,
+					height: 863,
+				}
+			]
 		},
 		twitter: {
 			card: 'summary_large_image',
 			title,
 			description,
-			images: [ogImage],
 		},
 	};
 }
 
-export default async function Blog({ params }) {
+export default function Blog({ params }) {
 	const post = allPosts.find((post) => post._raw.flattenedPath === params.slug);
 	return (
-		<div className="md:my-20 p-5 md:p-0">
-			<h1 className="font-bold text-4xl font-epilogue mb-5">
-				<Balancer>{post.title}</Balancer>
-			</h1>
-			<div className="grid grid-cols-[auto_1fr_auto] items-center mt-4 mb-8 font-mono text-sm max-w-[650px]">
-				<div className="bg-neutral-800 rounded-md px-2 py-1 tracking-tighter">{post?.publishedAt}</div>
-				<div className="h-[0.2em] bg-neutral-800 mx-2"></div>
-				<p className="font-mono text-sm text-neutral-500 tracking-tighter"><ViewCounter slug={post.slug} blogPage={true}></ViewCounter></p>
-			</div>
-			<Mdx code={post.body.code} />
+	<div className="md:my-20 p-5 md:p-0">
+		<h1 className="font-bold text-4xl font-epilogue mb-5">
+			<Balancer>{post.title}</Balancer>
+		</h1>
+		<div className="grid grid-cols-[auto_1fr_auto] items-center mt-4 mb-8 font-mono text-sm max-w-[650px]">
+			<div className="bg-neutral-800 rounded-md px-2 py-1 tracking-tighter">{post?.publishedAt}</div>
+			<div className="h-[0.2em] bg-neutral-800 mx-2"></div>
+			<p className="font-mono text-sm text-neutral-500 tracking-tighter"><ViewCounter slug={post.slug} blogPage={true}></ViewCounter></p>
 		</div>
+		<Mdx code={post.body.code} />
+	</div>
 	)
 }
