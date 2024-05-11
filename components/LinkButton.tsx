e64a76e6195e733367a82d4079bae60f19bc8a91
Kian <ristorikian@gmail.com>
2024-05-11 13:20:00 +0200

    Change img to next/Image

--- components/LinkButton.tsx
+++ components/LinkButton.tsx
@@ -1,7 +1,9 @@
+import Image from "next/image";
+
 export default function LinkButton(props: {img: string, name: string, link: string, alt: string}) {
   return (
     <a href={props.link} className="text-white bg-gray-800 font-medium rounded-lg text-sm px-2 py-2 text-center inline-flex items-center">
-        <img src={props.img} alt={props.alt} className="w-4 mr-2" />
+        <Image width={100} height={100} src={props.img} alt={props.alt} className="w-4 mr-2" />
         {props.name}
     </a>
   )
